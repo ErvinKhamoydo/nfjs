9b2bd400ce056426dcd77bb16b066b9716ac59dd
Ervin <hamoydoerik@gmail.com>
2020-09-11 18:26:20 +0300

    Added control functionality

--- js/script.js
+++ js/script.js
@@ -2,13 +2,6 @@ const score = document.querySelector('.score');
 const start = document.querySelector('.start');
 const gameArea = document.querySelector('.game-area');
 
-const car = document.createElement('div');
-car.classList.add('car');
-
-start.addEventListener('click', startGame);
-document.addEventListener('keydown', startRun);
-document.addEventListener('keyup', stopRun);
-
 const keys = {
     ArrowUp: false,
     ArrowDown: false,
@@ -22,18 +15,51 @@ const setting = {
     speed: 3
 };
 
+const car = document.createElement('div');
+car.classList.add('car');
+
 function startGame() {
     start.classList.add('hide');
     setting.start = true;
+
+    for (let i = 0; i < document.documentElement.clientHeight / 105; i++) {
+        const line = document.createElement('div');
+        line.classList.add('line');
+        line.style.top = (i * 100) + 'px';
+        line.y = i * 100;
+        gameArea.appendChild(line);
+    }
+
     gameArea.appendChild(car);
+
+    setting.x = car.offsetLeft;
+    setting.y = car.offsetTop;
 
     requestAnimationFrame(playGame);
 }
 
 function playGame() {
-    console.log('play');
-
     if (setting.start) {
+
+        if (keys.ArrowLeft && setting.x > 25) {
+            setting.x -= setting.speed;
+        }
+
+        if (keys.ArrowRight && setting.x < (gameArea.offsetWidth - car.offsetWidth / 2)) {
+            setting.x += setting.speed;
+        }
+
+        if (keys.ArrowUp && setting.y > 0) {
+            setting.y -= setting.speed;
+        }
+
+        if (keys.ArrowDown && setting.y < (gameArea.offsetHeight - car.offsetHeight - 1)) {
+            setting.y += setting.speed;
+        }
+
+        car.style.left = setting.x + 'px';
+        car.style.top = setting.y + 'px';
+
         requestAnimationFrame(playGame);
     }
 }
@@ -42,14 +68,14 @@ function startRun(event) {
     event.preventDefault();
 
     keys[event.key] = true;
-
-    console.log('start');
 }
 
 function stopRun(event) {
     event.preventDefault();
 
     keys[event.key] = false;
-
-    console.log('stop');
 }
+
+start.addEventListener('click', startGame);
+document.addEventListener('keydown', startRun);
+document.addEventListener('keyup', stopRun);
